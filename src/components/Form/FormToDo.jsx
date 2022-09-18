9a6fa6def16ec5df0bddd18e1928c17b587f5fa5
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-09-18 19:12:18 +0430

    create form todo

--- src/components/Form/FormToDo.jsx
+++ src/components/Form/FormToDo.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
-
+import styles from "./Form.module.css";
+import send from "../../images/Message_(Send).png"
 class FormToDo extends Component {
     render() {
         return (
-            <div>
-                
+            <div className={styles.container}>
+               <div className={styles.parentForm}>
+                   <form className={styles.form}>
+                        <input  type="text"/>
+                        <button>
+                            
+                            <img src={send} alt="send" />
+                        </button>
+                    </form>    
+               </div>
             </div>
         );
     }
