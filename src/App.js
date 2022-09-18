a3d6dafed92f449ef4de76386a99de8c8c961e11
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-09-18 18:38:42 +0430

    creat component form todo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import FormToDo from "./components/Form/FormToDo";
 function App() {
   return (
     <div>
-      
+       <FormToDo />
     </div>
   );
 }
---
+++ src/components/Form/FormToDo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FormToDo extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default FormToDo;
